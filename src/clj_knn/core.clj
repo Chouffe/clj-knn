143f02e6c3f48c7935acc035b4897728fa026794
Arthur Caillau <arthur@stuph.co>
2016-03-17 16:58:29 -0700

    Refactored predict function and implemented LWR regression

--- src/clj_knn/core.clj
+++ src/clj_knn/core.clj
@@ -1,20 +1,39 @@
 (ns clj-knn.core
-  (:require [clj-knn.utils :refer [mean take-at-least make-data]]
+  (:require [clj-knn.utils :refer [inverse lwr mode mean take-at-least make-data]]
             [clj-knn.distances :refer [euclidean manhattan]]))
 
-; TODO: Very inefficient - Use a heap and do not sort twice
+; TODO: Not very efficient - Use a min-heap O(k.lg(N)) instead of O (N.lg(N))
 (defn predict [prediction-policy]
+  "Prediction policy takes in a sequence of values
+   [[[X1 y1] distance], ... [[Xk yk] distancek]] and returns a y
+
+   Output: Returns a function that takes in the following keys:
+
+   - k: Integer
+   - distance-metric: similarity function - Mercer's Conditions
+   - data: all the data for the algo to be able to predict - lazy learning
+   - datapoint: a new datapoint to make a prediction on
+
+   - Output: the prediction on the new-datapoint"
   (fn [k distance-metric data datapoint]
     (let [distances (map (comp (partial distance-metric datapoint) first) data)
           k (->> distances sort (take-at-least k) count)]
-      (->> data
-           (sort-by (comp (partial distance-metric datapoint) first))
-           (map second)
+      (->> distances
+           (map vector data)
+           (sort-by second)
            (take k)
            prediction-policy))))
 
-(def classify (predict (comp first (partial sort-by second >) frequencies)))
-(def regression (predict mean))
+(def ys (partial map (comp second first)))
+(def distances (partial map second))
+
+(def classify (predict (comp mode ys)))
+(def regression (predict (comp mean ys)))
+; Locally Weighted Regression
+(def regression-2 (predict (comp (partial apply lwr)
+                                 (juxt ys
+                                       (comp (partial map inverse)
+                                             distances)))))
 
 ; Test Cases
 ; ------------
@@ -33,3 +52,13 @@
 (assert (= 42 (regression 3 euclidean data [4 2])))
 (assert (= 29 (regression 1 manhattan data [4 2])))
 (assert (= 71/2 (regression 3 manhattan data [4 2])))
+(assert (= 171/5 (regression-2 3 manhattan data [4 2])))
+(assert (<= 37 (regression-2 3 euclidean data [4 2]) 38))
+
+(def y-class [:slow :slow :fast :fast :fast :light-speed])
+(def data-class (make-data X y-class))
+
+(assert (= :slow (classify 1 euclidean data-class [4 2])))
+(assert (= :slow (classify 3 euclidean data-class [4 2])))
+(assert (= :fast (classify 3 manhattan data-class [5 6])))
+(assert (= :fast (classify 3 euclidean data-class [5 6])))
--- src/clj_knn/utils.clj
+++ src/clj_knn/utils.clj
@@ -1,15 +1,51 @@
 (ns clj-knn.utils)
 
 (defn square [x]
+  {:pre [(number? x)]
+   :post [(number? %)]}
   (* x x))
 
 (defn abs [x]
+  {:pre [(number? x)]
+   :post [(number? %)]}
   (max x (- x)))
 
 (defn mean [xs]
   {:pre [(seq xs) (every? number? xs)]
    :post [(number? %)]}
   (/ (reduce + xs) (count xs)))
+
+(assert (= 1 (mean [0 1 2])))
+(assert (= 0 (mean [0 0 0])))
+
+(defn mode [xs]
+  {:post [(or (get (set xs) %) (nil? %))]}
+  (let [freqs (frequencies xs)]
+    (first (reduce (fn [[k-max v-max] [k v]] (if (> v v-max) [k v] [k-max v-max]))
+                   (first freqs)
+                   (rest freqs)))))
+
+(assert (= :a (mode [:a :a :a :b :c])))
+(assert (= :d (mode (shuffle [:a :a :a :b :c :c :d :d :d :d]))))
+(assert (not (mode [])))
+
+(defn inverse [x]
+  (if (zero? x)
+    (Integer/MAX_VALUE)
+    (/ 1 x)))
+
+(assert (= 1 (inverse 1)))
+(assert (= 1/2 (inverse 2)))
+
+(defn lwr [xs weights]
+  (/ (reduce + (map * xs weights))
+     (reduce + weights)))
+
+(assert (= 1 (lwr [1 2] [1 0])))
+(assert (= 3/2 (lwr [1 2] [1 1])))
+
+(defn make-data [X y]
+  (mapv vector X y))
 
 (defn ^:private take-at-least-aux [n [x & xs :as coll] acc]
   (cond
@@ -23,7 +59,3 @@
 
 (defn take-at-least [n coll]
   (take-at-least-aux n coll []))
-
-(defn make-data [X y]
-  (mapv vector X y))
-
